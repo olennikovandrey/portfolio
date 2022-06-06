2dc76d4bc5b73336d2b8673eb70b44b737674ce9
olennikovandrey <olennikovandrey@mail.ru>
2022-06-06 18:03:52 +0300

    preload-page

--- portfolio/src/components/GeneralWrapper/GeneralWrapper.js
+++ portfolio/src/components/GeneralWrapper/GeneralWrapper.js
@@ -1,3 +1,4 @@
+import PreloadPage from "../PreloadPage/PreloadPage";
 import Header from "../Header/Header";
 import TopSection from "../TopSection/TopSection";
 import Skills from "../Skills/Skills";
@@ -5,26 +6,38 @@ import ExperienceEducation from "../ExperienceEducation/ExperienceEducation";
 import Projects from "../Projects/Projects";
 import Languages from "../Languages/Languages";
 import ContactMe from "../ContactMe/ContactMe";
-import React from "react";
+import "./general-wrapper.sass";
+import React, { useEffect, useState } from "react";
 
 function GeneralWrapper() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 8000);
+  }, []);
+
   return (
-    <div className="general-wrapper">
-      <Header />
-      <TopSection />
-      <Skills />
-      <ExperienceEducation
-        id="Education"
-        data="educationData"
-      />
-      <Projects />
-      <ExperienceEducation
-        id="Experience"
-        data="experienceData"
-      />
-      <Languages />
-      <ContactMe />
-    </div>
+    <>
+      { !isLoaded ? <PreloadPage /> :
+        <div className="general-wrapper">
+          <Header />
+          <TopSection />
+          <Skills />
+          <ExperienceEducation
+            id="Education"
+            data="educationData"
+          />
+          <Projects />
+          <ExperienceEducation
+            id="Experience"
+            data="experienceData"
+          />
+          <Languages />
+          <ContactMe />
+        </div>
+      }
+    </>
   );
 }
 
---
+++ portfolio/src/components/PreloadPage/PreloadPage.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import "./preload-page.sass";
+
+export default function PreloadPage() {
+  useEffect(() => {
+    setTimeout(() => {
+      typeLine();
+    }, 100);
+  });
+
+  const text = [
+    "Here you can find out\n",
+    "all the information you \n",
+    "need about me and, \n",
+    "if necessary, download a CV"
+  ];
+
+  let line = 0;
+  let count = 0;
+  let result = "";
+  function typeLine() {
+    const interval = setTimeout(
+      () => {
+        result += text[line][count];
+        document.querySelector("pre").innerHTML = result +"|";
+
+        count++;
+        if (count >= text[line].length) {
+          count = 0;
+          line ++;
+          if (line === text.length) {
+            clearTimeout(interval);
+            document.querySelector("pre").innerHTML = result;
+            return true;
+          }
+        }
+        typeLine();
+      }, getRandomInt(getRandomInt(180*2.5)));
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  return (
+    <>
+      <div className="typewriter">
+        <pre></pre>
+      </div>
+    </>
+  );
+}
