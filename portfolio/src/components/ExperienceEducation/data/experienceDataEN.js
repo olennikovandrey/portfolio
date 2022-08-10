c3911b673386bd08619a9039b64df457e277fa20
olennikovandrey <olennikovandrey@mail.ru>
2022-08-10 13:11:54 +0300

    exp

--- portfolio/src/components/ExperienceEducation/data/experienceDataEN.js
+++ portfolio/src/components/ExperienceEducation/data/experienceDataEN.js
@@ -1,37 +1,22 @@
 export const experienceDataEN = [
   {
-    years: "March 2021 - till now",
+    years: "March 2022 - till now",
     title: "Freelance",
     description: "Junior Front-end developer (ReactJS)"
   },
   {
-    years: "May 2020 - Nov 2021",
-    title: "Deputy commander of the military unit (Military forces)",
-    description: "Planning, organization and control of military unit activities"
-  },
-  {
-    years: "Oct 2018 - May 2020",
-    title: "Head of radio monitoring and radio suppression department (Military forces)",
+    years: "Oct 2018 - Nov 2021",
+    title: "Head of radio monitoring and radio suppression department",
     description: "Execution of special tasks"
   },
   {
     years: "Aug 2018 - Oct 2018",
-    title: "Head of information technical protection department (Military forces)",
-    description: "Organization, control and implementation of technical protection measures"
+    title: "Head of information technical protection department",
+    description: "Process management, planning, monitoring and training of personnel"
   },
   {
-    years: "Nov 2017 - Aug 2018",
-    title: "Battalion commander (Military forces)",
-    description: "Planning of day-to-day activities, management of subordinate subdivisions"
-  },
-  {
-    years: "Sept 2015 - Nov 2017",
-    title: "Company leader (Military forces)",
-    description: "Life management, education and training of the subordinate unit. Solving the problems of daily activities"
-  },
-  {
-    years: "Aug 2013 - Sept 2015",
-    title: "Platoon leader (Military forces)",
-    description: "Life management, education and training of the subordinate unit. Solving the problems of daily activities"
+    years: "Aug 2013 - Aug 2018",
+    title: "Unit commander",
+    description: "Management, planning, personnel training"
   },
 ];
